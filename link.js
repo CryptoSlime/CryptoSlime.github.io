7a49b8e5e8be2ee03d78ce556ae68c505bfa0727
CryptoSlime <91029352+CryptoSlime@users.noreply.github.com>
2021-09-28 20:07:52 -0700

    Add files via upload

---
+++ link.js
@@ -0,0 +1,14 @@
+function linkFunctionS(num) {
+  var x = document.getElementsByClassName("desS")[num].id;
+  window.open("https://www.nftexplorer.app/asset/" + x)
+}
+
+function linkFunctionR(num) {
+  var x = document.getElementsByClassName("desR")[num].id;
+  window.open("https://www.nftexplorer.app/asset/" + x)
+}
+
+function linkFunctionSR(num) {
+  var x = document.getElementsByClassName("desSR")[num].id;
+  window.open("https://www.nftexplorer.app/asset/" + x)
+}
